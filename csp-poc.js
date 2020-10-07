6d7ecb9e374b45e47065b7152955addf792e5dd1
jannunezz <jannunezz@gmail.com>
2020-10-06 21:44:53 -0400

    Update csp-poc.js

--- csp-poc.js
+++ csp-poc.js
@@ -1,4 +1,6 @@
+window.addEventListener('load', function () {
   var p = document.createElement("p");
   var t = document.createTextNode("Javascript hosted on GitHub here!");
   p.appendChild(t);
   document.body.appendChild(p);
+});
